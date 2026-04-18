a321bce20e84a14778aa767ffca6d7a7a7474ae1
ASUS <ASUS@LAPTOP-8IC9D7FL>
2026-04-18 16:16:28 +0700

    v1.3.

--- VinhKhanhTourDemo/Platforms/Android/MainActivity.cs
+++ VinhKhanhTourDemo/Platforms/Android/MainActivity.cs
@@ -1,7 +1,6 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
-using Android.Webkit;
 
 namespace VinhKhanhTourDemo.Platforms.Android
 {
@@ -16,30 +15,6 @@ namespace VinhKhanhTourDemo.Platforms.Android
     public class MainActivity : MauiAppCompatActivity
     {
         protected override void OnCreate(Bundle? savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-
-            Microsoft.Maui.Handlers.WebViewHandler.Mapper.AppendToMapping(
-                "EnableGeolocation",
-                (handler, view) =>
-                {
-                    if (handler.PlatformView is not global::Android.Webkit.WebView webView) return;
-                    webView.Settings.JavaScriptEnabled = true;
-                    webView.Settings.SetGeolocationEnabled(true);
-                    webView.Settings.SetGeolocationDatabasePath(
-                        global::Android.App.Application.Context.FilesDir?.Path);
-                    webView.SetWebChromeClient(new GeoWebChromeClient());
-                });
-        }
-    }
-
-    public class GeoWebChromeClient : global::Android.Webkit.WebChromeClient
-    {
-        public override void OnGeolocationPermissionsShowPrompt(
-            string? origin,
-            global::Android.Webkit.GeolocationPermissions.ICallback? callback)
-        {
-            callback?.Invoke(origin, true, false);
-        }
+            => base.OnCreate(savedInstanceState);
     }
 }
